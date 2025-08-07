e8fb645b0413678825a9620cfe1b9f68e4100209
ADELA JIMENEZ HERVAS <adelajihervas@gmail.com>
2025-08-07 18:19:30 +0200

    Cleaning repo. Remove ignored folders from version control

--- build/ur/colcon_command_prefix_build.sh
+++
@@ -1,7 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/adela/TFM_AdelaJimenez/install/ur_dashboard_msgs/share/ur_dashboard_msgs/package.sh"
-. "/home/adela/TFM_AdelaJimenez/install/ur_description/share/ur_description/package.sh"
-. "/home/adela/TFM_AdelaJimenez/install/ur_controllers/share/ur_controllers/package.sh"
-. "/home/adela/TFM_AdelaJimenez/install/ur_moveit_config/share/ur_moveit_config/package.sh"
-. "/home/adela/TFM_AdelaJimenez/install/ur_robot_driver/share/ur_robot_driver/package.sh"
-. "/home/adela/TFM_AdelaJimenez/install/ur_calibration/share/ur_calibration/package.sh"
--- build/ur_calibration/colcon_command_prefix_build.sh
+++
@@ -1,5 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/adela/TFM_AdelaJimenez/install/ur_dashboard_msgs/share/ur_dashboard_msgs/package.sh"
-. "/home/adela/TFM_AdelaJimenez/install/ur_description/share/ur_description/package.sh"
-. "/home/adela/TFM_AdelaJimenez/install/ur_controllers/share/ur_controllers/package.sh"
-. "/home/adela/TFM_AdelaJimenez/install/ur_robot_driver/share/ur_robot_driver/package.sh"
